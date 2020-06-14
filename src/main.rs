55dc87fe4037b3c3e07b1f5d94ae323aeae016f3
Stephen Hassard <steve@hassard.net>
2020-06-14 07:01:47 -0700

    fixup blacklist checking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::fs;
 
-static BLACKLIST: [&'static str; 3] = ["Cargo.lock", "Cargo.yaml", "Makefile"];
+/// check if a filename is blacklisted from being renamed
+fn blacklisted(filename: &str) -> bool {
+    match filename {
+        "Cargo.lock" => true,
+        "Cargo.toml" => true,
+        "Makefile" => true,
+        _ => false,
+    }
+}
 
 /// returns a cleaned up path name
 fn fix_name(path: String) -> String {
@@ -15,12 +23,10 @@ fn main() {
     'outer: for path in paths {
         let old_path = path.unwrap().path();
         let old_filename = old_path.file_name().unwrap().to_str().unwrap();
-        for item in &BLACKLIST {
-            if *item == old_filename {
+        if blacklisted(old_filename) {
                 println!("matched blacklist: {}", old_filename);
                 continue 'outer;
             }
-        }
         let new_name = fix_name(String::from(old_filename));
         if old_filename != new_name {
             println!("{} -> {}", old_filename, new_name);
